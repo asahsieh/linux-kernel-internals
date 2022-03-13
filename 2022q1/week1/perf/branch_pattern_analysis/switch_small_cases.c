614fa21c81e6e0659ca1ba2adef855eb06cc76e4
ASaHsieh <kchs.tw@gmail.com>
2022-03-13 23:10:19 +0800

    Increase number of execution to making more samples on the source code to be measured

--- 2022q1/week1/perf/branch_pattern_analysis/switch_small_cases.c
+++ 2022q1/week1/perf/branch_pattern_analysis/switch_small_cases.c
@@ -3,15 +3,16 @@
  *   [Branch Patterns, Using GCC](https://tinyurl.com/annm23jh)
  */
 
+#define N 5000000
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 
-void call_0() { printf("call_0() is called\n"); }
-void call_1() { printf("call_1() is called\n"); }
-void call_2() { printf("call_2() is called\n"); }
-void call_3() { printf("call_3() is called\n"); }
+void call_0() {}
+void call_1() {}
+void call_2() {}
+void call_3() {}
 
 void test_0( int a )
 {
@@ -36,25 +37,9 @@ void test_0( int a )
 }
 
 int main (int argc, char *argv[]) {
-    /* long int strtol (const char* str, char** endptr, int base); */
-    int in_val = 0;
-    char str[100];
+    int i;
 
-    // Put code which more likely to be executed in if block
-    if (argc == 2)
-    {
-        strcpy(str, argv[1]);
-    }
-    else
-    {
-        printf("Please give an input value: ");
-        scanf("%s", str);
-    }
-
-    in_val = atoi(str); 
-    printf("The input value is: %d.\n", in_val);
-
-	test_0(in_val);
+    for (i = 0; i < N; i++) test_0(i%4);
 
     return EXIT_SUCCESS; 
 }
