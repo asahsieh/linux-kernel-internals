13ce3dd6b63afea85a963fe8e7d45be9e1328d3c
ASaHsieh <kchs.tw@gmail.com>
2022-04-06 14:54:53 +0800

    Porting pre-commit flow from lab0-c
    The GIT_HOOKS need to be install in top-level directory, related files
    are moved to top-level directory and the installation by Makefile is
    replaced by manual installation.
    
    Execute clang-format on the memchr_improv.c. Use printf to observe
    values of formula in both_odd_swar().

--- 2022q1/week8/test1/memchr_improv.c
+++ 2022q1/week8/test1/memchr_improv.c
@@ -2,19 +2,26 @@
 #include <stdint.h>
 #include <stdio.h>
 
-bool both_odd(uint32_t x, uint32_t y) {
+bool both_odd(uint32_t x, uint32_t y)
+{
     return (x & 1) && (y & 1);
-
 }
 
+/*
+ * Only need to observe bit 0 to determine whether input `xy` is odd.
+ * Why the SWAR_ODD_MASK need to do `(1L << 32)`
+ */
+
 static uint64_t SWAR_ODD_MASK = (1L << 32) + 1;
-bool both_odd_swar(uint64_t xy) {
+bool both_odd_swar(uint64_t xy)
+{
+    printf("xy=0x%lx, SWAR_ODD_MASK=0x%lx\n", xy, SWAR_ODD_MASK);
     return (xy & SWAR_ODD_MASK) == SWAR_ODD_MASK;
 }
 
-inline uint64_t bit_compound(uint32_t x, uint32_t y) {
+inline uint64_t bit_compound(uint32_t x, uint32_t y)
+{
     return ((0L + x) << 32) | ((y + 0L) & (-1L >> 32));
-
 }
 
 int main()
@@ -23,5 +30,4 @@ int main()
     int y = 9012345;
     uint64_t xy = bit_compound(x, y);
     printf("%d, %d\n", both_odd(x, y), both_odd_swar(xy));
-
 }
