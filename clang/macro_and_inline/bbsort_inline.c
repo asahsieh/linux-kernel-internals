700df30a6d2d74eb6c3a7f82d438a62689432a4b
ASaHsieh <kchs.tw@gmail.com>
2022-05-02 10:20:48 +0800

    Add pre-processor-conditional for C99 6.7.4 para 3
    To test the inline function and the object with static storage duration
    is in the same translation unit. The added object is `swap_cnt` which
    stores the number of calls to swap().
    
    The test works correctly, it may not be the example code of the
    paragraph in C99

--- clang/macro_and_inline/bbsort_inline.c
+++ clang/macro_and_inline/bbsort_inline.c
@@ -5,11 +5,14 @@
 inline static void swap(int *, int *); /* swap的宣告 */
 extern void bbsort(int *, size_t);     /* bbsort的宣告 */
 
+#ifdef CONS_3_SAME_TRANS_UNIT
+static int swap_cnt = 0;
+#endif
+
 int main(int argc, char *argv[])
 {
     int A[] = {7, 2, 4, 6, 9, 8};
     size_t i = 0, n = sizeof(A) / sizeof(int);
-    swap(A, A + 1); /* main::swap */
     bbsort(A, n);
 
     for (i = 0; i < n; ++i)
@@ -32,11 +35,17 @@ inline void bbsort(int *A, size_t n)
 {
     size_t i, j;
 
+#ifndef CONS_3_SAME_TRANS_UNIT
+    static int swap_cnt = 0;
+    printf("CONS_3_SAME_TRANS_UNIT isn't defined.\n");
+#endif
     for (i = 0; i < n; i++) {
         for (j = i + 1; j < n; j++) {
             if (A[i] > A[j]) {
                 swap(A + i, A + j);
+                swap_cnt++;
             }
         }
     }
+    printf("bbsort::swap_cnt=%d\n", swap_cnt);
 }
