37ac354743b044b95224a18905f3f4ae74dabd82
ASaHsieh <kchs.tw@gmail.com>
2022-06-06 14:19:16 +0800

    Display values of inputs (i,j) on each case
    For easily observe the changes of inputs.

--- practice/clang/macro_and_inline/statement_expresssion_in_gnu_c/macro_eval_twice.c
+++ practice/clang/macro_and_inline/statement_expresssion_in_gnu_c/macro_eval_twice.c
@@ -5,36 +5,40 @@
 int main(int argc, char *argv[])
 {
     int i = 2, j = 3, k, l, tmp;
-    // max(i++, j++);
-    // ((i++) > (j++) ? (i++) : (j++));
-    printf("=== To test the macro with inputs causing \"pitfall\"");
+
+    printf("=== To test the macro with inputs causing \"pitfall\"\n");
     printf(", `max(i++, j++)`===\n");
 
     do {
         k = i;
         l = j;
+
         printf("Before the comparison:\ti=%d, j=%d\n", k, l);
         if ((i++) > (j++)) {
             printf("In the comparison:\ti=%d, j=%d\n", i, j);
             (i++);
             if (i == (k + 2)) {
+                printf("After the comparison:\ti=%d, j=%d\n\n", i, j);
                 printf("[WARN] The expression are evaluated twice.\n");
-                printf("The larger value (%d) is incremented twice to (%d)\n\n",
+                printf("The larger value `i` (%d) is incremented twice to (%d)",
                        k, i);
                 goto EXANGE_INPUTS;
             }
-
         } else {
             printf("In the comparison:\ti=%d, j=%d\n", i, j);
             (j++);
             if (j == (l + 2)) {
+                printf("After the comparison:\ti=%d, j=%d\n\n", i, j);
                 printf("[WARN] The expression are evaluated twice.\n");
-                printf("The larger value (%d) is incremented twice to (%d)\n\n",
+                printf("The larger value `j` (%d) is incremented twice to (%d)",
                        l, j);
                 goto EXANGE_INPUTS;
             }
         }
-        printf("The macro is evaluated safely.\n\n");
+        printf("[SAFE] The larger value %c is keep unchanged, ",
+               (k > l ? i : j));
+        printf("so The macro is evaluated safely.\n\n");
+
         goto TEST_MACRO_SAFE;
 
         // Exange the two inputs to observe the else statement
@@ -42,20 +46,25 @@ int main(int argc, char *argv[])
         tmp = j;
         j = i;
         i = tmp;
+
+        printf("\n\n");
     } while (i > j);
 
 TEST_MACRO_SAFE:
-    printf("=== To test the macro with inputs make it \"safe\", ");
+    printf("=== To test the macro with inputs make it \"safe\",\n");
     printf("`max(i,j)` ===\n");
+
     k = i;
     l = j;
+
     printf("Before the comparison:\ti=%d, j=%d\n", k, l);
     if ((i) > (j)) {
         printf("In the comparison:\ti=%d, j=%d\n", i, j);
         if (i == (k + 2)) {
+            printf("After the comparison:\ti=%d, j=%d\n\n", i, j);
             printf("[WARN] The expression are evaluated twice.\n");
-            printf("The larger value (%d) is incremented twice to (%d)\n\n", k,
-                   i);
+            printf("The larger value `i` (%d) is incremented twice to (%d)\n\n",
+                   k, i);
 
             goto END_OF_TEST;
         }
@@ -63,14 +72,17 @@ TEST_MACRO_SAFE:
     } else {
         printf("In the comparison:\ti=%d, j=%d\n", i, j);
         if (j == (l + 2)) {
+            printf("After the comparison:\ti=%d, j=%d\n\n", i, j);
             printf("[WARN] The expression are evaluated twice.\n");
-            printf("The larger value (%d) is incremented twice to (%d)\n\n", l,
-                   j);
+            printf("The larger value `j` (%d) is incremented twice to (%d)\n\n",
+                   l, j);
 
             goto END_OF_TEST;
         }
     }
-    printf("The macro is evaluated safely.\n");
+    printf("After the comparison:\ti=%d, j=%d\n\n", i, j);
+    printf("[SAFE] The larger value %c is keep unchanged", (k > l ? 'i' : 'j'));
+    printf(",\nso The macro is evaluated safely.\n\n");
 
 END_OF_TEST:
     return 0;
