f81f52f1d65ef5347d23691f105084d73b8b632d
ASaHsieh <kchs.tw@gmail.com>
2022-08-02 15:10:56 +0800

    Observe struct with members in double type

--- practice/clang/usr-def_type/mem-align.c
+++ practice/clang/usr-def_type/mem-align.c
@@ -6,16 +6,30 @@
  *
  */
 #include <stdio.h>
-/* Definition of Student */
-typedef struct Student {
+/* Definition of Student_flo */
+typedef struct Student_flo {
     int ID;               /* int, 4 bytes */
     char gender;          /* char, 1 byte */
     float height, weight; /* two float types, 8 bytes */
-} Student;
+} Student_flo;
+
+/* Definition of Student_dou */
+typedef struct Student_dou {
+    int ID;                /* int, 4 bytes */
+    char gender;           /* char, 1 byte */
+    double height, weight; /* two double types, 16 bytes */
+} Student_dou;
 
 /* main function */
 int main(int argc, char *argv[])
 {
-    printf("%zu\n", sizeof(Student)); /* should be 16, not 13 */
+    printf("%zu\n", sizeof(Student_flo));          /* should be 4x4 on the word
+                                                      size >= 4 bytes, not 13 */
+    printf("%zu\n", sizeof(struct Student_dou));   /* should be 6x4 on the word
+                                                      size >= 4 bytes, not 21 */
+    printf("%zu\n", _Alignof(Student_flo));        /* should be 4 on
+                                                      word size == 4 bytes    */
+    printf("%zu\n", _Alignof(struct Student_dou)); /* should be 8 on
+                                                      word size == 8 bytes    */
     return 0;
 }
